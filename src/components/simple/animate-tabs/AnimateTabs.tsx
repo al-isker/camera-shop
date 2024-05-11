c7263f3f6c79862779c80ef580d1b1430d7f592b
Alisker Desktop <balisker@mail.ru>
2024-05-11 19:06:22 +0300

    added Animate tabs

---
+++ src/components/simple/animate-tabs/AnimateTabs.tsx
@@ -0,0 +1,47 @@
+import {FC, MouseEventHandler, ReactNode} from 'react';
+import { LayoutGroup, motion } from "framer-motion";
+
+import s from './animate-tabs.module.css';
+
+interface AnimateTabsProps {
+  options:  string[]
+  active: string
+  setActive: Function
+}
+
+export const AnimateTabs:FC<AnimateTabsProps> = ({options, active, setActive}) => {
+  return (
+    <div className={s.tab_list}>
+      {options.map((option: string) => (
+        <AnimateTabsItem
+          isActive={option === active}
+          onClick={() => setActive(option)}
+        >
+          {option}
+        </AnimateTabsItem>
+      ))}
+    </div>
+  );
+};
+
+
+interface AnimateTabsItemProps {
+  children: ReactNode
+  isActive: boolean
+  onClick: MouseEventHandler
+}
+
+const AnimateTabsItem:FC<AnimateTabsItemProps> = ({children, isActive, onClick}) => {
+  return (
+    <button
+      className={s.tab}
+      type="button"
+      onClick={onClick}
+    >
+      {isActive && (
+        <motion.div layoutId="tab" className={s.tab_motion}></motion.div>
+      )}
+      <span className={s.tab_title}>{children}</span>
+    </button>
+  )
+}
